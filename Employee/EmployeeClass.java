29825496c6b6150338eac53f8911d93167345c4a
Tyler Vickers <67883080+VickersCode@users.noreply.github.com>
2023-07-24 14:55:35 -0700

    First Commit

---
+++ Employee/EmployeeClass.java
@@ -0,0 +1,6 @@
+package Employee;
+public class EmployeeClass {
+    public static void main(String[] args) {
+        Employee susan = new Employee("Susan", 47899, "Accounting", "Vice President");
+    }
+}
